e208595b995d94dc348aaf8b010086d8e883de08
kaihrii leriina <litetencent5@gmail.com>
2025-07-12 16:48:03 +0530

    front end added

--- src/main/java/com/example/SpringAi/controller/AiController.java
+++ src/main/java/com/example/SpringAi/controller/AiController.java
@@ -3,11 +3,15 @@ package com.example.SpringAi.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
+@CrossOrigin(origins = "http://localhost:5173/")
 public class AiController {
 	
 	 private ChatClient chatClient;
